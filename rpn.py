835a30ee89a419cd6077ffdd7c651971cac1bd7c
Zach Boyle <zboyle@umich.edu>
2018-10-26 15:20:48 -0400

    added error throwing for invalid input

--- rpn.py
+++ rpn.py
@@ -21,15 +21,21 @@ def calculate(arg):
                 result = val1 / val2
 
             stack.append(result)
-
+    if len(stack) > 1 :
+        raise ValueError('too many arguments on the stack')
+    
+    
     return stack[0]
 
 
 
 def main():
     while True:
-        result = calculate(input('rpn calc> '))
-        print(result)
+        try:
+            result = calculate(input('rpn calc> '))
+            print(result)
+        except ValueError:
+            pass
 
 if __name__ == '__main__':
     main()
