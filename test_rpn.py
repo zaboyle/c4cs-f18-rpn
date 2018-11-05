20c9e235f338e7352bb37853cde3ac6fb79e7532
zboyle <zboyle@umich.edu>
2018-11-05 11:18:03 -0500

    forgot to add main in test_rpn.py

--- test_rpn.py
+++ test_rpn.py
@@ -9,6 +9,14 @@ class TestBasics(unittest.TestCase):
     def test_sub(self):
         result = rpn.calculate('4 3 -')
         self.assertEqual(1, result)
+    
+    def test_divide(self):
+        result = rpn.calculate('6 3 /')
+        self.assertEqual(2, result)
+    
+    def test_multi(self):
+        result = rpn.calculate('4 5 *')
+        self.assertEqual(20, result)
 
     def test_exponent(self):
         result = rpn.calculate('2 4 ^')
@@ -17,4 +25,8 @@ class TestBasics(unittest.TestCase):
     def test_toomany(self):
         with self.assertRaises(ValueError):
             result = rpn.calculate('1 2 3 +')
+
+if __name__ == '__main__':
+    unittest.main()
+
 
